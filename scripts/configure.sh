ead52bcb9668f5508ca30b0e4e499f22deba5921
Shashank S <talsperre@users.noreply.github.com>
2017-09-29 22:47:15 +0530

    Update configure.sh

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -6,9 +6,9 @@ echo "        proxy_set_header Host \$host;" >> /etc/nginx/sites-enabled/cs
 echo "        proxy_set_header X-Real-IP \$remote_addr;" >> /etc/nginx/sites-enabled/cs
 echo "    }" >> /etc/nginx/sites-enabled/cs
 echo "}" >> /etc/nginx/sites-enabled/cs
-echo "import sys, os" > /var/www/content-server/wsgi.py
-echo "from runtime.rest.app import create_app" >> /var/www/content-server/wsgi.py
-echo "from runtime.config import app_config as config" >> /var/www/content-server/wsgi.py
-echo "application = create_app(config)" >> /var/www/content-server/wsgi.py
-echo "if __name__ == '__main__':" >> /var/www/content-server/wsgi.py
-echo "    application.run(host='0.0.0.0')" >> /var/www/content-server/wsgi.py
+echo "import sys, os" > /var/www/content-server/code/wsgi.py
+echo "from runtime.rest.app import create_app" >> /var/www/content-server/code/wsgi.py
+echo "from runtime.config import app_config as config" >> /var/www/content-server/code/wsgi.py
+echo "application = create_app(config)" >> /var/www/content-server/code/wsgi.py
+echo "if __name__ == '__main__':" >> /var/www/content-server/code/wsgi.py
+echo "    application.run(host='0.0.0.0')" >> /var/www/content-server/code/wsgi.py
